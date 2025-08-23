d5730af0224b73f9c21b2da4b7e9068e0a11d4b8
JadsonCardoso <jadsonc190@gmail.com>
2025-08-22 21:18:22 -0300

    testes na API via  HTTP e adicionando na Pipe

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// server.js
+// server.js - é quem expoe a app via http
 const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
---
+++ test/external/transferExternal.test.js
@@ -0,0 +1,24 @@
+// Bibliotecas
+const request = require('supertest');
+const { expect } = require('chai');
+
+
+// Testes via HTTP
+describe('Transfer', () => {
+    describe('POST /transfer', () => {
+         it('Quando informo remetente e destinatario inexistentes recebo 400', async () => {
+            const resposta = await request('http://localhost:3000/api-docs/') // Passamos a URL da API ao invés do APP
+                .post('/transfer')
+                .send({
+                    from: "jadson",
+                    to: "yasmin",
+                    amount: 100
+                });
+
+            expect(resposta.status).to.equal(400);
+            expect(resposta.body).to.have.property('error', 'Usuário remetente ou destinatário não encontrado')
+        });
+
+    });
+
+});
